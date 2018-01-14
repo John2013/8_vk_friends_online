cf2bf977eb48bf1ad95dd205e9bb34657de0409f
John2013 <NaWasington@yandex.ru>
2018-01-14 16:42:40 +0300

    сделал загрузку данных только онлайн друзей, хотя сомнительно, всё таки теперь два запроса вместо одного

--- vk_friends_online.py
+++ vk_friends_online.py
@@ -1,15 +1,19 @@
 from getpass import getpass
+from pprint import pprint
+
 import vk
 
 APP_ID = 6331864
 
 
 def get_user_login():
-    return input('Введите логин: ')
+    return '+79997912013'
+    # return input('Введите логин: ')
 
 
 def get_user_password():
-    return getpass('Введите пароль: ')
+    return '86421357Jt'
+    # return getpass('Введите пароль: ')
 
 
 def get_online_friends(login, password):
@@ -17,10 +21,15 @@ def get_online_friends(login, password):
         app_id=APP_ID,
         user_login=login,
         user_password=password,
+        scope='friends'
     )
     api = vk.API(session)
-    friends = api.friends.get(order='name', fields='online', name_case='nom')
-    return [friend for friend in friends if friend['online'] == 1]
+    friend_ids = api.friends.getOnline()
+    friends = api.users.get(
+        user_ids=','.join(map(str, friend_ids)),
+        fields='first_name,last_name',
+    )
+    return friends
 
 
 def output_friends_to_console(friends_online):
